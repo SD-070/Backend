e9d75c1438711a999aec61f2ee56f4d82147c76b
Garrett Lander <garrett.jlander@gmail.com>
2026-02-04 12:10:54 +0100

    updated user controllers

--- chapter09-restful-apis/07-middleware-error-handling-lecture/src/controllers/users.ts
+++ chapter09-restful-apis/07-middleware-error-handling-lecture/src/controllers/users.ts
@@ -14,18 +14,16 @@ const getAllUsers: RequestHandler = async (req, res) => {
 	res.json(users);
 };
 const createUser: RequestHandler<{}, {}, UserInputType> = async (req, res) => {
-	if (!req.body) {
-		return res
-			.status(400)
-			.json({ error: 'Name, image URL, owner, and quote are required' });
-	}
+	if (!req.body)
+		throw new Error('First name, last name, and email are required', {
+			cause: { status: 400 }
+		});
 	const { firstName, lastName, email } = req.body;
 
-	if (!firstName || !lastName || !email) {
-		return res
-			.status(400)
-			.json({ error: 'First name, last name, and email are required' });
-	}
+	if (!firstName || !lastName || !email)
+		throw new Error('First name, last name, and email are required', {
+			cause: { status: 400 }
+		});
 
 	const newUser = await User.create({
 		firstName,
@@ -37,56 +35,66 @@ const createUser: RequestHandler<{}, {}, UserInputType> = async (req, res) => {
 };
 const getUserById: RequestHandler<{ id: string }> = async (req, res) => {
 	const { id } = req.params;
-	if (!isValidObjectId(id)) {
-		return res.status(400).json({ error: 'Invalid ID' });
-	}
+	if (!isValidObjectId(id))
+		throw new Error('Invalid ID', {
+			cause: { status: 400 }
+		});
 	const user = await User.findById(id);
 
-	if (!user) return res.status(404).json({ error: 'User not found' });
+	if (!user)
+		throw new Error('User not found', {
+			cause: { status: 404 }
+		});
 	res.json(user);
 };
 const updateUser: RequestHandler<{ id: string }, {}, UserInputType> = async (
 	req,
 	res
 ) => {
-	if (!req.body) {
-		return res
-			.status(400)
-			.json({ error: 'Name, image URL, owner, and quote are required' });
-	}
+	if (!req.body)
+		throw new Error('First name, last name, and email are required', {
+			cause: { status: 400 }
+		});
 
 	const { id } = req.params;
 	const { firstName, lastName, email } = req.body;
 
-	if (!firstName || !lastName || !email) {
-		return res
-			.status(400)
-			.json({ error: 'First name, last name, and email are required' });
-	}
+	if (!firstName || !lastName || !email)
+		throw new Error('First name, last name, and email are required', {
+			cause: { status: 400 }
+		});
 
-	if (!isValidObjectId(id)) {
-		return res.status(400).json({ error: 'Invalid ID' });
-	}
+	if (!isValidObjectId(id))
+		throw new Error('Invalid ID', {
+			cause: { status: 400 }
+		});
 	const user = await User.findByIdAndUpdate(
 		id,
 		{ firstName, lastName, email } satisfies UserInputType,
 		{ new: true }
 	);
 
-	if (!user) return res.status(404).json({ error: 'User not found' });
+	if (!user)
+		throw new Error('User not found', {
+			cause: { status: 404 }
+		});
 
 	res.json(user);
 };
 const deleteUser: RequestHandler<{ id: string }> = async (req, res) => {
 	const { id } = req.params;
 
-	if (!isValidObjectId(id)) {
-		return res.status(400).json({ error: 'Invalid ID' });
-	}
+	if (!isValidObjectId(id))
+		throw new Error('Invalid ID', {
+			cause: { status: 400 }
+		});
 
 	const found = await User.findByIdAndDelete(id);
 
-	if (!found) return res.status(404).json({ error: 'User not found' });
+	if (!found)
+		throw new Error('User not found', {
+			cause: { status: 404 }
+		});
 
 	res.json({ message: 'User deleted' });
 };
