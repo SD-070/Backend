15cfc8da6e979968cc42185cb5009290b837c0f1
Garrett Lander <garrett.jlander@gmail.com>
2026-02-09 12:02:49 +0100

    comments and starter code

--- chapter10-custom-auth-flow/01-authentication-i/auth-server/src/controllers/auth.controller.ts
+++ chapter10-custom-auth-flow/01-authentication-i/auth-server/src/controllers/auth.controller.ts
@@ -35,12 +35,22 @@ export const register: RequestHandler = async (req, res) => {
 };
 
 export const login: RequestHandler = async (req, res) => {
-  // TODO: Implement user login
-  // Check if the user exists in the database
-  // Compare the password from the request with the hash in your db
-  // Send an Error "Incorrect credentials" if either no user is found (invalid email) or the password is incorrect
-  // Issue tokens and put them into cookies
-  // Also store the refresh token in your db
+  // get email and password from request body
+  //
+  // query the DB to find user with that email
+  //
+  // if not user is found, throw a 401 error and indicate invalid credentials
+  //
+  // compare the password to the hashed password in the DB with bcrypt
+  // const match = await bcrypt.compare(password, user.password);
+  //
+  // if match is false, throw a 401 error and indicate invalid credentials
+  //
+  // delete all Refresh Tokens in DB where userId is equal to _id of user
+  //
+  // generate refresh and access tokens
+  //
+  // send generic success message, refreshToken, and accessToken in body of response
 };
 
 export const refresh: RequestHandler = async (req, res) => {
@@ -61,8 +71,37 @@ export const logout: RequestHandler = async (req, res) => {
 };
 
 export const me: RequestHandler = async (req, res, next) => {
-  // TODO: Implement a me handler
-  // Get the access token and use it to retrieve the user's data
-  // Make sure that the token is valid and not expired
-  // When expired, send a WWW-Authenticate Header with a 'token_expired' payload
+  // get accessToken from request headers
+  const authHeader = req.header('authorization');
+  console.log('authHeader:', authHeader);
+
+  const accessToken = authHeader && authHeader.split(' ')[1];
+
+  // if there is no access token throw a 401 error with an appropriate message
+
+  try {
+    // verify the access token
+    // const decoded = jwt.verify(accessToken, ACCESS_JWT_SECRET) as jwt.JwtPayload;
+    // console.log(decoded)
+    //
+    // if decoded.sub if falsy, throw a 403 error and indicate Invalid or expired token
+    //
+    // query the DB to find user by id that matches decoded.sub
+    //
+    // throw a 404 error if no user is found
+    //
+    // send generic success message and user info in response body
+  } catch (error) {
+    // if error is an because token was expired, call next with a 401 and `ACCESS_TOKEN_EXPIRED' code
+    if (error instanceof jwt.TokenExpiredError) {
+      next(
+        new Error('Expired access token', {
+          cause: { status: 401, code: 'ACCESS_TOKEN_EXPIRED' }
+        })
+      );
+    } else {
+      // call next with a new 401 Error indicated invalid access token
+      next(new Error('Invalid access token.', { cause: { status: 401 } }));
+    }
+  }
 };
